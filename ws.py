c6df192e74910a15bfc56978711bd58128b983be
Your Name <amboudilaye@email.com>
2024-06-28 19:20:24 -0700

    Merge branch 'master' of https://github.com/GitAmboudilaye/ambouchat.git

--- ws.py
+++ ws.py
@@ -19,4 +19,7 @@ def handle_message(msg):
     send(msg, broadcast=True)
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0')
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
